e6d2e62716266983c12d0d8e16a363e6d9b98186
Eraw <mael.arnaud@gmail.com>
2014-09-27 09:57:21 +0200

    Amélioration algorithme Exo TD2N2

--- TD2/ExoTD2N2.rb
+++ TD2/ExoTD2N2.rb
@@ -15,10 +15,9 @@
       e.afficher("Saisissez deux nombres.")
       cl.saisir(a, b)
         // a = a0, b = b0
-      si          
-        (a > 0 et b < 0) ou (a < 0 et b > 0 ) : e.afficher("Le produit des deux nombres est négatif")
-      sinon
-        : e.afficher("Le produit des deux nombres est positif ou nul")
+      si (a > 0 et b < 0) ou (a < 0 et b > 0 )    
+        alors e.afficher("Le produit des deux nombres est négatif")
+        sinon e.afficher("Le produit des deux nombres est positif ou nul")
       fsi
 =end
 
