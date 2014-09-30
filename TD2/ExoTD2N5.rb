9634c092fe20a2c3dd459924ff52bd1f19a15ec9
Eraw <mael.arnaud@gmail.com>
2014-09-30 14:43:58 +0200

    Modification du calcul des mois

--- TD2/ExoTD2N5.rb
+++ TD2/ExoTD2N5.rb
@@ -9,25 +9,26 @@
 
     Lexique
       Données
-        Anc : Entier // ancienneté de l'individu, en années, donnée utilisateur
-        AncMin : Entier // Ancienneté de l'individu, en mois, donnée utilisateur
-        Age : Entier // age de l'individu, en années, donnée utilisateur
+        Anc : Réel // ancienneté de l'individu, en années, donnée utilisateur
+        Age : Réel // age de l'individu, en années, donnée utilisateur
         EstC : Booleen // définit si l'individu est cadre ou non
       Variables
-        JdC : Entier // nombre de jours de congés dans l'année
+        AncMin : Réel // Ancienneté de l'individu en mois, donnée intermédiaire
       Périphériques
         cl : clavier // périphérique d'entrée
         e : écran // périphérique de sortie  
         
     Algorithme
-        // Anc = ?, AncMin = ?, Age = ?, EstC = ?, JdC = ?
+        // Anc = ?, AncMin = ?, Age = ?, EstC = ?
         
       e.afficher("Veuillez entrer le nombre d'années d'ancienneté de la personne")
       cl.saisir(Anc)
       
-      si Anc = 0
-      alors e.afficher("Veuillez entrer le nombre de mois d'ancienneté de la personne") ; cl.saisir(AncMin)
+      si Anc < 1
+      alors AncMin ← pent(Anc*12)
       fsi
+      
+          // Anc = Anc0, AncMin = (AncMin0 ou ?), Age = ?, EstC = ?
       
       e.afficher ("Veuillez entrer l'âge de la personne")
       cl.saisir(Age)
@@ -35,28 +36,27 @@
       e.afficher ("Veuillez indiquer le statut de la personne")
       cl.saisir(EstC)
       
-          // Anc = Anc0, AncMin = (AncMin0 ou ?), Age = Age0, EstC = EstC0, JdC = ?
+          // Anc = Anc0, AncMin = (AncMin0 ou ?), Age = Age0, EstC = EstC0
           
       selon
-        Anc < 1                                                                                                           : e.afficher(AncMin*2, " jours de congé")
-        Anc > 1 et (Anc < 3 ou EstC = faux ou Age < 35)                                                                   : e.afficher("28 jours de congé")
+                                                                                                                  Anc < 1 : e.afficher(AncMin*2, " jours de congé")
+                                                                          Anc > 1 et (Anc < 3 ou EstC = faux ou Age < 35) : e.afficher("28 jours de congé")
         (EstC = vrai et (Anc >= 3 et (Age >= 35 et Age < 45))) ou (EstC = vrai et ( Anc >= 3 et (Anc <= 5 et Age >= 35))) : e.afficher(28+2, " jours de congé")
-        EstC = vrai et (Anc >= 5 et Age >= 45)                                                                            : e.afficher(28+4, " jours de congé")
+                                                                                   EstC = vrai et (Anc >= 5 et Age >= 45) : e.afficher(28+4, " jours de congé")
       fselon
 =end
 
 # ruby
 
 puts "Veuillez entrer le nombre d'années d'ancienneté de la personne"
-anc = gets.chomp.to_i
+anc = gets.chomp.to_f
 
-if anc == 0
-  puts "Veuillez entrer le nombre de mois d'ancienneté de la personne"
-  ancMin = gets.chomp.to_i
+if anc < 1
+  ancMin = (anc*12).to_i
 end
 
 puts "Veuillez entrer l'âge de la personne"
-age = gets.chomp.to_i
+age = gets.chomp.to_f
 
 puts "La personne est-elle cadre ?"
 mlop = gets.chomp
