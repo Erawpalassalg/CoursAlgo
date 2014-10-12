b4658c508e5a228dfae0d3d5588cbdfdf76eced2
Eraw <mael.arnaud@gmail.com>
2014-10-12 11:54:25 +0200

    Exo 4 Sans ruby

---
+++ TD4/Exo4.rb
@@ -0,0 +1,35 @@
+#encoding: utf-8
+
+=begin
+
+  lexique principal
+    m : machine-tracés
+    c : Entier // donnée
+  
+  algorithme principal
+    m.Vider
+    m.Gauche(120)
+    m.Av(C/2)
+    m.Droite(40)
+    m.Baisser
+    m.av(C/2)
+    m.Droite(80)
+    m.Av(C)
+    m.Droite(80)
+    m.Av(C)
+    m.Droite(80)
+    m.Av(C)
+    m.Droite(80)
+    m.Av(C/2)
+    m.Droite(120)
+    m.Av(C/2)
+    m.Gauche(80)
+    m.Av(C/2)
+    m.Gauche(80)
+    m.Av(C/2)
+    m.Gauche(80)
+    m.Av(C/2)
+    m.Lever
+    m.Dir(0)
+    
+=end
