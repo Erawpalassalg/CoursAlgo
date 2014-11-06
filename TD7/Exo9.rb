a92a105fe99d4ab35ebf5a491455d27b3db6a595
Eraw <mael.arnaud@gmail.com>
2014-11-06 16:43:31 +0100

    Exo 9 avec  Ruby

--- TD7/Exo9.rb
+++ TD7/Exo9.rb
@@ -1,2 +1,52 @@
 #encoding: utf-8
 
+=begin
+
+  Lexique
+    f : Fichier de caractères // donnée initiale, fichier lu
+    wasThatAnL : Booleen // indique si la lettre précédente était un "L"
+    n : Entier // nombre de "LE" présent dans la séquence parcourue
+    e : écran // périphérique de sortie
+    
+  Algorithme
+    wasThatAnL ← faux
+    n ← 0
+    f.LirePremier
+    
+    Tantque non f.FDF
+    
+      Selon f.EC wasThatAnL n
+        f.EC = 'L' : wasThatAnL ← vrai
+        f.EC = 'E' et wasThatAnL : n ← n+1
+        Autrement : wasThatAnL ← faux
+      fSelon
+      
+      f.LireSuivant
+      
+    fTantque
+  
+    e.afficher("Nombre de LE :" n)
+  
+=end
+
+#Ruby
+
+f = File.open("Exo9Test.txt", "r")
+wasThatAnL = false
+n = 0
+
+f.each_line do |x|
+  i = 0
+  while i < x.length
+    if x[i] == 'L'
+      wasThatAnL = true
+    elsif x[i] == 'E' and wasThatAnL
+      n += 1
+    else
+      wasThatAnL = false
+    end
+    i += 1
+  end
+end
+
+puts "Nombre de LE : #{n}"
