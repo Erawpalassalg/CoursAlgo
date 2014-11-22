29fba9bd9b82e8144ab04a71b7e453ec682a1b88
Eraw <mael.arnaud@gmail.com>
2014-11-22 07:59:19 +0100

    Exo 1 avec ruby corrigés

--- TD8/Exo1.rb
+++ TD8/Exo1.rb
@@ -11,35 +11,32 @@ Lexique
   
 Algorithme
   prec ← ' '
-  toto ← faux
-  
   f.LirePremier
   
-  Tantque non f.FDF oualors f.EC = ' ' faire
+  Tantque non f.FDF etpuis f.EC = ' ' faire
     f.LireSuivant
   fTantque
   // f.FDF oualors f.EC != ' '
   
   Si non f.FDF
-  Alors totocar ← f.EC ; toto vrai
+  Alors totocar ← f.EC ; toto vrai ; 
+      Tantque non f.FDF et toto = vrai faire
+        Si prec = ' ' et f.EC != ' '
+        Alors Si totocar != f.EC
+              Alors toto ← faux
+              fSi
+        fSi
+        prec ← f.EC
+        f.LireSuivant
+      fTantque
+      // f.FDF ou toto = faux
+      
+      Selon toto
+        toto     : e.afficher("Le fichier est composé d'un tautogramme")
+        non toto : e.afficher("Le fichier n'est pas composé d'un tautogramme")
+      fSelon
+  Sinon e.afficher("Le fichier est vide")
   fSi
-  
-  Tantque non f.FDF et toto = vrai faire
-    Si prec = ' ' et f.EC != ' '
-    Alors Si totocar != f.EC
-          Alors toto ← faux
-          fSi
-    fSi
-    prec ← f.EC
-    f.LireSuivant
-  fTantque
-  // f.FDF ou toto = faux
-  
-  Selon toto
-    toto     : e.afficher("Le fichier est composé d'un tautogramme")
-    non toto : e.afficher("Le fichier n'est pas composé d'un tautogramme")
-  fSelon
-  
 =end
 
 a = File.readlines("Exo1test.txt")
@@ -47,6 +44,6 @@ i = 0
 toto = false
 a.each do |x|
   totocar = x[0][0]
-  x.split.each {|y| y[0] == totocar ? toto = true  : toto = false ; break}
+  x.split.each {|y| y[0] == totocar ? toto = true  : (toto = false ; break)}
 end
 toto ? (puts "Le fichier contient un tautogramme") : (puts "Le fichier ne contient pas de tautogramme.")
